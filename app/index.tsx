ec5a9481b10d3d14ee8822237d3adb1169199184
ganyunhee <eunicenellgan@gmail.com>
2024-09-07 19:37:42 +0900

    Edit index file

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,6 @@
 import { Stack, Link } from 'expo-router';
 
+import { View, Text } from 'react-native';
 import { Button } from '~/components/Button';
 import { Container } from '~/components/Container';
 import { ScreenContent } from '~/components/ScreenContent';
