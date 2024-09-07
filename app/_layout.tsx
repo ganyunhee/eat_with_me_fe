fb718be6fc046e79a5e9cff72bf94402aae34fe7
William East <wjbetech@gmail.com>
2024-09-07 22:08:18 +0900

    Added more create-event fields

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,7 +5,6 @@ import { Stack } from 'expo-router';
 export default function Layout() {
   return (
     <Stack>
-      <Stack.Screen name="index" />
       <Stack.Screen name="home" />
       <Stack.Screen name="create-event" />
       <Stack.Screen 
--- app/create-event.tsx
+++ app/create-event.tsx
@@ -1,13 +1,83 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { ReactEventHandler, useState } from 'react'
+import { StyledView, StyledText, StyledInput, StyledButton } from "~/components/StyledComponents"
+import { Link } from "expo-router"
+
+interface FormProps {
+  name: string,
+  description: string,
+  date: string,
+  time: string,
+  location: string,
+}
 
 const createEvent = () => {
+
+  // state for inputs
+  const [inputs, setInputs] = useState<FormProps>({
+    name: '',
+    description: '',
+    date: '',
+    time: '',
+    location: ''
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    const { name, value } = e.target;
+    setInputs((prevInputs) => ({
+      ...prevInputs,
+      [name]: value
+    }))
+  };
+
   return (
-    <View>
-      <Text>createEvent</Text>
-    </View>
+    <StyledView className="bg-white flex-1 items-center justify-center pb-4">
+      <StyledText className="text-3xl font-bold mb-4">
+        Create an Event
+      </StyledText>
+      <StyledView className="w-full max-w-sm">
+        <StyledInput placeholder="Add an event name..."></StyledInput>
+      </StyledView>
+      <StyledView className="w-full max-w-sm">
+        <StyledInput placeholder="Number of people to invite..."></StyledInput>
+      </StyledView>
+    </StyledView>
   )
 }
 
 export default createEvent
 
+//     <StyledView className="bg-white flex-1 items-center justify-center pb-4">
+//         <StyledText className="text-3xl font-bold mb-4">Login</StyledText>
+//         <StyledView className="w-full max-w-sm bg-red-300">
+//         <StyledText className="mb-2 mt-4">Email</StyledText>
+//         <StyledInput 
+//           placeholder="Input your email here"
+//           onChangeText={setEmail}
+//           value={email}
+//         />
+//         <StyledText className="mb-2 mt-4">Password</StyledText>
+//         <StyledInput 
+//           placeholder="Input your password here"
+//           secureTextEntry={true}
+//           onChangeText={setPassword}
+//           value={password}
+//         />
+//         <Link
+//           href="/home"
+//           asChild
+//           onPress={handleLogin}
+//         >
+//           <StyledButton className="mt-6">
+//             Login
+//           </StyledButton>
+//         </Link>
+//         </StyledView>
+//     </StyledView>
+//   )
+// }
+
+// export default login
+
+// const styles = StyleSheet.create({})
