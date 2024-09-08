bd99befbf18776fafe5f964a3b41b54cf3ff25d9
ganyunhee <eunicenellgan@gmail.com>
2024-09-08 10:19:20 +0900

    Add map trackers for map view

--- components/KakaoMap.tsx
+++ components/KakaoMap.tsx
@@ -7,26 +7,47 @@ const html = `
 <html>
 <head>
     <meta charset="utf-8">
-    <title>지도 생성하기</title>
-    
+    <title>단일 마커 표시하기</title>
 </head>
 <body>
-<!-- 지도를 표시할 div 입니다 -->
-<div id="map" style="width:100%; height:90vh; "></div>
+    <div id="map" style="width:100%;height:100vh;"></div>
 
-<script type="text/javascript" src="https://dapi.kakao.com/v2/maps/sdk.js?appkey=8cca17be8a53180771b9d97e346b2db6"></script>
-<script>
-var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
-    mapOption = { 
-        center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
-        level: 3 // 지도의 확대 레벨
-    };
+    <script type="text/javascript" src="//dapi.kakao.com/v2/maps/sdk.js?appkey=e66ac44b0a3ae1a499911f4b45bafaaa"></script>
+    <script>
+    var mapContainer = document.getElementById('map'), 
+        mapOption = { 
+            center: new kakao.maps.LatLng(37.498004414546934, 127.02770621963765), 
+            level: 3 
+        }; 
 
-// 지도를 표시할 div와  지도 옵션으로  지도를 생성합니다
-var map = new kakao.maps.Map(mapContainer, mapOption); 
-</script>
+    var map = new kakao.maps.Map(mapContainer, mapOption);
+
+    var positions = [ 
+        new kakao.maps.LatLng(37.499590490909185, 127.0263723554437),
+        new kakao.maps.LatLng(37.499427948430814, 127.02794423197847),
+        new kakao.maps.LatLng(37.498553760499505, 127.02882598822454),
+        new kakao.maps.LatLng(37.497625593121384, 127.02935713582038),
+        new kakao.maps.LatLng(37.49646391248451, 127.02675574250912),
+        new kakao.maps.LatLng(37.49629291770947, 127.02587362608637),
+        new kakao.maps.LatLng(37.49754540521486, 127.02546694890695)                
+    ];
+
+    var imageSrc = 'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/marker_red.png', 
+        imageSize = new kakao.maps.Size(64, 69), 
+        imageOption = {offset: new kakao.maps.Point(27, 69)}; 
+    
+    var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption);
+
+    for (var i = 0; i < positions.length; i ++) {
+        var marker = new kakao.maps.Marker({
+            map: map,
+            position: positions[i],
+            image: markerImage 
+        });
+    }
+    </script>
 </body>
-</html>   
+</html>
 `;
 
 export default function KakaoMap() {
